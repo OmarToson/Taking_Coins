5037728502b68496373a03802705e3ea7865be00
Omar Toson Khater <44278568+OmarToson@users.noreply.github.com>
2018-10-19 03:18:33 +0200

    Edit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position; //distance between camera and player
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {   //runs every frame like update
+        transform.position = player.transform.position + offset;  // player position + distance
+	}
+    //in unity we press on camera --> cameracontroller --> drag player and drop on player in cameracontroller
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    public Text countText;
+    public Text winText;
+
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Vertical");
+        float moveVertical = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);  //x , y ,z
+
+        rb.AddForce(movement * speed);  //i don't put value pecause i won't it static 
+        //i can press on player --> inspector--> player controller --> change speed
+    }
+
+    void OnTriggerEnter(Collider other)     //when the object touch collider
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);  //set player checkbox from inspector = false 
+
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 15)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
+
+
